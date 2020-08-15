c116ca5c3121e7d4a1e1c0be57811f7844567c80
root <root@bsmt-pit-frmn01.int.group53.net>
2020-08-15 19:47:26 +0000

    bugfixes

--- manifests/varlink.pp
+++ manifests/varlink.pp
@@ -9,42 +9,42 @@ define podman::varlink (
   String $socket      = '/run/podman/io.podman',
   String $user        = 'root',
   String $group       = 'root',
-  String $socket_mode = '0750'.
+  String $socket_mode = '0750',
 ) {
 
   # write temp file config
-  $working_directory = join(split($socket, "/")[0:-1], ",")
+  $working_directory = join(split($socket, "/")[0,-1], "/")
   $contents = "d ${working_directory} ${socket_mode} ${user} ${group}"
   file { "/etc/tmpfiles.d/podman.conf" :
-      ensure   => $ensure,
-      contents => "${contents}",
-      owner    => "root",
-      group    => "root"
+      ensure  => $ensure,
+      content => "${contents}",
+      owner   => "root",
+      group   => "root"
   }
 
   # systemd helpers
   exec { "podman-systemd-refresh":
-      command  => "systemctl daemon-reload",
-      refresh  => true,
+      command     => "/usr/bin/systemctl daemon-reload",
+      refreshonly => true,
   }
   exec { "podman-systemd-tmpfiles-refresh" :
-      command  => "systemd-tmpfiles --create --no-pager",
-      refresh  => true,
+      command     => "/usr/bin/systemd-tmpfiles --create --no-pager",
+      refreshonly => true,
   }
 
   # install service file
   file { "/etc/systemd/system/io.podman.socket" :
-      ensure   => $ensure,
-      content  => template("${module_name}/io.podman.socket.erb"),
-      notify   => [Exec["podman-systemd-refresh"], Exec["podman-systemd-tmpfiles-refresh"]]
+      ensure  => $ensure,
+      content => template("${module_name}/io.podman.socket.erb"),
+      notify  => [Exec["podman-systemd-refresh"], Exec["podman-systemd-tmpfiles-refresh"]]
   }
 
   if $ensure == 'present' {
-  	  $svc_ensure = 'running'
-  	  $svc_enable = 'true'
+      $svc_ensure = 'running'
+      $svc_enable = true
   } else {
-  	  $svc_ensure = 'stopped'
-  	  $svc_enable = 'false'
+      $svc_ensure = 'stopped'
+      $svc_enable = false
   }
 
   service { "io.podman.socket" :
