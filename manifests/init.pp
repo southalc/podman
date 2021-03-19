2f0a5af553ef397e3ffe116c79634dbbbbab864f
Trey Dockendorf <tdockendorf@osc.edu>
2021-03-19 14:19:00 -0400

    Actually make podman-docker optional

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,6 +9,9 @@
 # @param podman_docker_pkg
 #   The name of the podman-docker package (default 'podman-docker')
 #
+# @param manage_podman_docker
+#   Set whether podmand-docker should be managed
+#
 # @param pods
 #   A hash of pods to manage using [`podman::pod`](#podmanpod)
 #
@@ -75,7 +78,8 @@
 class podman (
   String $podman_pkg,
   String $skopeo_pkg,
-  Optional[String] $podman_docker_pkg,
+  String $podman_docker_pkg,
+  Boolean $manage_podman_docker    = true,
   Boolean $manage_subuid           = false,
   Boolean $match_subuid_subgid     = true,
   String $file_header              = '# FILE MANAGED BY PUPPET',
--- manifests/install.pp
+++ manifests/install.pp
@@ -13,11 +13,11 @@
 class podman::install (
   String $podman_pkg                  = $podman::podman_pkg,
   String $skopeo_pkg                  = $podman::skopeo_pkg,
-  Optional[String] $podman_docker_pkg = $podman::podman_docker_pkg,
+  String $podman_docker_pkg = $podman::podman_docker_pkg,
 ){
   ensure_resource('Package', $podman_pkg, { 'ensure' => 'installed' })
   ensure_resource('Package', $skopeo_pkg, { 'ensure' => 'installed' })
-  if $podman_docker_pkg { ensure_resource('Package', $podman_docker_pkg, { 'ensure' => 'installed' }) }
+  if $podman::manage_podman_docker { ensure_resource('Package', $podman_docker_pkg, { 'ensure' => 'installed' }) }
 
   if $podman::manage_subuid {
     Concat { '/etc/subuid':
