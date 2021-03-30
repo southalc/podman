2832b398137b6f338cd6bb2449d28121809e4f4f
Chris Southall <southallc@gmail.com>
2021-03-29 21:46:42 -0500

    added subid, volumes, and containers to unit tests

--- spec/classes/podman_spec.rb
+++ spec/classes/podman_spec.rb
@@ -4,22 +4,25 @@ describe 'podman' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
-#      let(:params) do
-#        {
-#          manage_subuid: false,
-#          match_subuid_subgid: true,
-#          file_header: '# FILE MANAGED BY PUPPET',
-#          nodocker: 'absent',
-#          subid: {},
-#          pods: {},
-#          volumes: {},
-#          images: {},
-#          containers: {},
-#        }
-#      end
+      let(:params) do
+        {
+          manage_subuid: true,
+        }
+      end
+      let(:pre_condition) do
+        'User { "user1":
+          uid  => 10001,
+          gid  => 10001,
+          home => "/home/user1",
+        }
+        User { "user2":
+          uid  => 10002,
+          gid  => 10002,
+          home => "/home/user2",
+        }'
+      end
 
       it { is_expected.to contain_class('podman::install') }
-
       it { is_expected.to compile }
     end
   end
