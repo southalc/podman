6d132e7c765fdb0a4851c31c53bc82daadd45095
Chris Southall <southallc@gmail.com>
2021-09-17 22:36:40 -0500

    add exec_env to support proxy image pulls

--- manifests/image.pp
+++ manifests/image.pp
@@ -16,6 +16,11 @@
 #   the user must also be defined as a Puppet resource and must include the
 #   'uid', 'gid', and 'home'
 #
+# @param exec_env
+#   Optional array of environment variables used when the container image is
+#   pulled.  Useful for defining a proxy for downloads. For example:
+#   ["HTTP_PROXY=http://${proxy_fqdn}:3128", "HTTPS_PROXY=http://${proxy_fqdn}:3128"]
+#
 # @example
 #   podman::image { 'my_container':
 #     image => 'my_container:tag',
@@ -26,10 +31,10 @@
 #
 define podman::image (
   String $image,
-  String $ensure    = 'present',
-  Hash $flags       = {},
-  String $user      = '',
-  Hash $exec_extras = {},
+  String $ensure = 'present',
+  Hash $flags    = {},
+  String $user   = '',
+  Array $exec_env = [],
 ){
   require podman::install
 
@@ -54,9 +59,9 @@ define podman::image (
     $exec_defaults = {
       path        => '/sbin:/usr/sbin:/bin:/usr/bin',
       environment => [
-        "HOME=${User[$user]['home']}",
-        "XDG_RUNTIME_DIR=/run/user/${User[$user]['uid']}",
-      ],
+          "HOME=${User[$user]['home']}",
+          "XDG_RUNTIME_DIR=/run/user/${User[$user]['uid']}",
+        ] + $exec_env,
       cwd         => User[$user]['home'],
       provider    => 'shell',
       user        => $user,
@@ -68,22 +73,23 @@ define podman::image (
   } else {
     $exec_defaults = {
       path        => '/sbin:/usr/sbin:/bin:/usr/bin',
+      environment => $exec_env,
     }
   }
-  $full_exec = $exec_defaults + $exec_extras
+
   case $ensure {
     'present': {
       Exec { "pull_image_${title}":
         command => "podman image pull ${_flags} ${image}",
         unless  => "podman image exists ${image}",
-        *       => $full_exec,
+        *       => $exec_defaults,
       }
     }
     'absent': {
       Exec { "pull_image_${title}":
         command => "podman image pull ${_flags} ${image}",
         unless  => "podman rmi ${image}",
-        *       => $full_exec,
+        *       => $exec_defaults,
       }
     }
     default: {
